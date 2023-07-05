17a5bcad77c92a5609f419329e0b6625b12e3f5b
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-05 19:02:52 +0200

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-   h3 {
-     color: darkgreen;
-   }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'hello-world';
