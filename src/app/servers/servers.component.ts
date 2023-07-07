7ca0aef2fc7f80d36467d092e875510228da798e
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-07 12:03:21 +0200

    ngIf directive to output data conditionally

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,8 @@ import {Component, OnInit} from '@angular/core';
 export class ServersComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = 'TestServer';
+  serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit{
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
