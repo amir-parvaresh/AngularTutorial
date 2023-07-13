02917ee586c1ec303a8741c7c3d8527fb510858d
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-13 16:46:10 +0200

    Custom Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,20 @@ ngOnInit() {
 constructor() {
 }
   serverElements = [{type: 'server', name: 'TestServer', content: 'this is test server'}];
-  newServerName = '';
-  newServerContent = '';
+
+  onAddServer(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+      });
+  }
+
+  onAddBlueprint(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  ngOnInit() {
+  }
+
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  newServerName: any;
+  newServerContent: any;
+
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
+  }
+}
