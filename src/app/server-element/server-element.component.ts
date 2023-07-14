05eb610d2bf92f48810ff706d0445b90c0f777c9
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-14 15:00:01 +0200

    Disabling View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None //This will turn off the view Encapsulation. All the CSSs will have global
+  // scope now (red label will be applied to all the labels)
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
