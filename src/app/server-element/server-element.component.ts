f07b8f4ff275f483923bf97227418b13bac3eee8
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-17 09:57:20 +0200

    Getting access to ng-Content with @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,13 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,12 +16,18 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None //This will turn off the view Encapsulation. All the CSSs will have global
   // scope now (red label will be applied to all the labels)
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterContentInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @ContentChild('contentParagraph', {static: true}) contentParagraph!: ElementRef ;
 
   ngOnInit() {
+    console.log(this.contentParagraph.nativeElement.textContent);
   }
   constructor() {
     this.element = {type: 'hi', content: 'hi', name: 'hi'};
+  }
+  ngAfterContentInit() {
+    // This will be only shown here
+    console.log(this.contentParagraph.nativeElement.textContent);
   }
 }
