633e1451124ce303e66203d76ad5980c797a689e
aparvare <amir-hossein.parvaresh@capgemini.com>
2023-07-18 16:09:20 +0200

    ngFor and ngIf Recap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumber = [2, 4, 6];
+  onlyOdd = false;
 }
